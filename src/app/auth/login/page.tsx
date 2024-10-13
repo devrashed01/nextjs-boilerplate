6833be9d7feac6ecb3581efe52abf179523b44e5
eahea.ratan <eahea.ratan@strativ.se>
2024-10-13 22:15:09 +0600

    merged

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,27 +2,20 @@
 
 import { Button, Form, Input, Typography } from "antd";
 
-
 const { Title } = Typography;
 
 export default function page() {
-  const handleSubmit = (values:string) => {
-    console.log(values)
+  const handleSubmit = (values: string) => {
+    console.log(values);
   };
 
   return (
     <Form onFinish={handleSubmit}>
       <Title level={3} style={{ marginBottom: 0 }}>
-            Ant Design (With Sub Components)
-          </Title>
-      <Input
-        name="username"
-        placeholder="Username"
-      />
-      <Input
-        name="email"
-        placeholder="Email"
-      />
+        Ant Design (With Sub Components)
+      </Title>
+      <Input name="username" placeholder="Username" />
+      <Input name="email" placeholder="Email" />
       <Button>Login button</Button>
     </Form>
   );
